6795935bddc4f402d6619bddca56c3ebc503821e
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-10-28 00:17:50 +0000

    Refactor: showScoreScreen() and add comments

--- entities.js
+++ entities.js
@@ -194,7 +194,7 @@ class Background extends Entity {
                 this.bottomImage.y -= 1;
                 this.bottomImage.update(delta);
 
-                let finalBrightness = 0.6;
+                let finalBrightness = 0.4;
 
                 // Reduce background brightness as the bottom bank image and score is revealed.
                 this.filter.brightness(1 - (finalBrightness / this.bottomImage.height) *  (HEIGHT - this.bottomImage.y));
@@ -221,8 +221,9 @@ class Background extends Entity {
     }
 
     showScoreScreen() {
+        let textColour = "0xffffff";
         let gameOver = new TextEntity(0, 0, "Game Over", new PIXI.TextStyle({
-            fill: "#ffffff",
+            fill: textColour,
             fontSize: 50,
             fontWeight: "bold"
         }));
@@ -231,31 +232,48 @@ class Background extends Entity {
         let scoreDetailsText = `Fish caught: ${gameObject.fishCaught}\nLongest streak: ${gameObject.longestStreak}\nFish missed: ${gameObject.fishMissed}\nBoots hit: ${gameObject.bootsHit}`;
 
         let score = new TextEntity(0, 0, "Score: " + gameObject.score, new PIXI.TextStyle({
-            fill: "#ffffff",
+            fill: textColour,
             fontSize: 30,
             fontWeight: "bold",
         }));
 
         let scoreDetails = new TextEntity(0, 0, scoreDetailsText.trim(), new PIXI.TextStyle({
-            fill: "#ffffff",
+            fill: textColour,
             fontSize: 20,
             align: "center",
         }));
-        
-        let scoreSprite = score.getSprite();
-        let scoreDetailsSprite = scoreDetails.getSprite();
+
+        // Create PIXI container
+        let scoreScreenContainer = new PIXI.Container();  
+
+        // Game over text
         let gameOverSprite = gameOver.getSprite();
+        gameOverSprite.y = 0;
+        scoreScreenContainer.addChild(gameOverSprite);  
 
-        gameOverSprite.y = this.backgroundContainer.height / 4 - gameOverSprite.height;
-        this.centerSpriteX(gameOverSprite);
-
+        // Score text - 20px below game over text
+        let scoreSprite = score.getSprite();
         scoreSprite.y = gameOverSprite.y + gameOverSprite.height + 20;
-        this.centerSpriteX(scoreSprite);
+        scoreScreenContainer.addChild(scoreSprite);  
 
+        // Score details text - 15px below score text
+        let scoreDetailsSprite = scoreDetails.getSprite();
         scoreDetailsSprite.y = scoreSprite.y + scoreSprite.height + 15;
-        this.centerSpriteX(scoreDetailsSprite);
+        scoreScreenContainer.addChild(scoreDetailsSprite);  
 
-        app.stage.addChild(gameOverSprite, scoreSprite, scoreDetailsSprite);
+        // Position the score screen container 
+        scoreScreenContainer.y = this.backgroundContainer.height / 2.5 - scoreScreenContainer.height / 2;
+
+        // Center the container horizontally
+        this.centerSpriteX(scoreScreenContainer);  
+        
+        // Center the sprites within the container horizontally
+        gameOverSprite.x = scoreScreenContainer.width / 2 - gameOverSprite.width / 2;
+        scoreSprite.x = scoreScreenContainer.width / 2 - scoreSprite.width / 2;
+        scoreDetailsSprite.x = scoreScreenContainer.width / 2 - scoreDetailsSprite.width / 2;       
+
+        // Add scoreScreen container to the stage
+        app.stage.addChild(scoreScreenContainer);
         gameObject.showScoreScreen = true;
     }
 
