7dacf17318bc3ec317c5e84e49664836e15ca9ea
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-10-28 21:20:24 +0000

    optimise: Entity hook update + ensure sprite garabge collection

--- entities.js
+++ entities.js
@@ -76,6 +76,8 @@ class Entity {
     removeEntity() {
         if (this.sprite != undefined) {
             app.stage.removeChild(this.sprite);
+            this.sprite.destroy();
+            this.sprite = undefined;
         }
         entities.splice(entities.indexOf(this), 1);
     }
@@ -95,7 +97,6 @@ class Entity {
 class TextEntity extends Entity {
     constructor(x, y, text, style) {
         super(x, y, 0, 0);
-        this.text = text;
         this.sprite = new PIXI.Text(text, style);
 
         this.sprite.x = this.x;
@@ -455,32 +456,15 @@ class PlayerHook extends Entity {
         this.hookLine.update(this.x, this.y);
 
         if (this.y < this.desiredY - this.speed / 2) {
-            if (this.y + this.speed + this.height > gameObject.bounds.maxY) {
-                this.y = gameObject.bounds.maxY - this.height;
-            } else {
-                this.y += this.speed;
-            }
-
+            this.y = Math.min(this.y + this.speed, gameObject.bounds.maxY - this.height);
         } else if (this.y > this.desiredY + this.speed / 2) {
-            if (this.y - this.speed - this.height < gameObject.bounds.minY) {
-                this.y = gameObject.bounds.minY;
-            } else {
-                this.y -= this.speed;
-            }
+            this.y = Math.max(this.y - this.speed, gameObject.bounds.minY);
         }
-
+        
         if (this.x < this.desiredX - this.speed / 2) {
-            if (this.x + this.speed + this.width > gameObject.bounds.maxX) {
-                this.x = gameObject.bounds.maxX - this.width;
-            } else {
-                this.x += this.speed;
-            }
+            this.x = Math.min(this.x + this.speed, gameObject.bounds.maxX - this.width);
         } else if (this.x > this.desiredX + this.speed / 2) {
-            if (this.x - this.speed - this.width < gameObject.bounds.minX) {
-                this.x = gameObject.bounds.minX;
-            } else {
-                this.x -= this.speed;
-            }
+            this.x = Math.max(this.x - this.speed, gameObject.bounds.minX);
         }
     }
 
@@ -515,13 +499,19 @@ class HookLine extends Entity {
     update(x, y) {
         super.update();
 
-        this.x = x + this.playerHook.width / 2;
-        this.y = y;
+        const newX = x + this.playerHook.width / 2;
+        const newY = y;
 
-        this.lineGraphics.clear();
-        this.lineGraphics.lineStyle(this.lineThickness, 0x000000, 1);
-        this.lineGraphics.moveTo(this.START_X, this.START_Y);
-        this.lineGraphics.lineTo(this.x, this.y)
+        // If moved, update the line
+        if (this.x !== newX || this.y !== newY) {
+            this.x = newX;
+            this.y = newY;
+
+            this.lineGraphics.clear();
+            this.lineGraphics.lineStyle(this.lineThickness, 0x000000, 1);
+            this.lineGraphics.moveTo(this.START_X, this.START_Y);
+            this.lineGraphics.lineTo(this.x, this.y);
+        }
     }
 }
 
